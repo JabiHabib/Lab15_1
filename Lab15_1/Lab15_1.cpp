f99da9753dc68a7ee4bf45f98539c6792f5a38fa
JabibHabib <kubusalsa@gmail.com>
2023-12-29 14:18:56 +0200

    Добавьте файлы проекта.

---
+++ Lab15_1/Lab15_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "Ukr");
+    int nums[2];
+
+    printf("Введiть два числа: ");
+    for (int i = 0; i < 2; i++) {
+        scanf_s("%d", &nums[i]);
+    }
+
+    if (nums[0] > nums[1]) {
+        printf("Перше число бiльше: %d\n", nums[0]);
+    }
+    else {
+        printf("Обидва числа: %d %d\n", nums[0], nums[1]);
+    }
+
+    return 0;
+}
